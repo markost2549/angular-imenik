a3afafabc38b98a33b226210c721dfd0820c09d0
Marko Stojkovic <markost2549@gmail.com>
2020-09-09 14:02:03 +0200

    model update

--- src/app/imenik/imenik.model.ts
+++ src/app/imenik/imenik.model.ts
@@ -1,6 +1,8 @@
 export class Imenik {
   constructor(
     public id: number,
+    public odeljenje: string,
+    public lokacija: string,
     public name: string,
     public phoneNumber: string
   ) {}
